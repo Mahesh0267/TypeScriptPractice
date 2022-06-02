add601ee764105c1e1c706bb757785468511e406
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-06-02 14:29:23 +0530

    sort with type guard

---
+++ sort/src/Sorter.ts
@@ -0,0 +1,31 @@
+class Sorter {
+    //   collection: number[] | string;
+  
+    //   constructor(collection: number[] | string) {
+    //     this.collection = collection;
+    //   }
+  
+    //Another way to defined parameter in constructor
+    constructor(public collection: ) {}
+    sort(): void {
+      const { length } = this.collection;
+      for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+          //If collection is array of numbers
+        //   if (this.collection instanceof Array) {  //type guard
+            if (this.collection[j] > this.collection[j + 1]) {
+              const leftHand = this.collection[j];
+            //   console.log(leftHand);
+              this.collection[j] = this.collection[j + 1];
+            //   console.log(this.collection[j]);
+              this.collection[j + 1] = leftHand;
+            }
+        //   }
+          //if collection is array of string
+        //   if(typeof this.collection === 'string'){
+              
+        //   }
+        }
+      }
+    }
+  }
