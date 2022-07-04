a15e09f389a2ace948f1bd98ff60996a6a618f3e
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-07-04 13:05:31 +0530

    jsx render

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -6,8 +6,21 @@ interface AppProps{
     color?:string;
 }
 
-class App extends React.Component<AppProps> {
-    state = {counter:0};
+interface AppState{
+    counter:number;
+}
+
+// const App =(props: AppProps):JSX.Element =>{
+//     return <div>{props.color}</div>
+// }
+
+class App extends React.Component<AppProps,AppState> {
+    
+    constructor(props:AppProps){
+        super(props);
+
+        this.state ={counter:0};
+    }
 
     onIncrement = () : void =>{
         this.setState({counter:this.state.counter + 1})
